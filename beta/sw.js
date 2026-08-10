18d89baa8bd42026881a94a0eab55c04f4995cb0
flanhenrique <walflanribeiro@gmail.com>
2026-08-10 18:51:08 -0400

    chore(beta): atualizar cache para tema do detalhamento

--- beta/sw.js
+++ beta/sw.js
@@ -8,7 +8,7 @@
 // Política atual: cache exclusivamente do shell estático same-origin,
 // declarado em ASSETS. Nenhuma outra requisição é armazenada.
 
-const CACHE = "volt-beta-shell-v65";
+const CACHE = "volt-beta-shell-v66";
 
 const ASSETS = [
   "./",

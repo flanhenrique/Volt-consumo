24ebd17969f107384de9e2f8e12d0904ee62edde
flanhenrique <walflanribeiro@gmail.com>
2026-08-10 16:47:11 -0400

    feat(beta): load guided account experience

--- beta/beta-v3.js
+++ beta/beta-v3.js
@@ -19,6 +19,7 @@ import "./energy-detail.js";
 import "./locality-context.js";
 import "./platform-users.js";
 import "./hide-organization-context.js";
+import "./guided-experience.js";
 
 const REDUCED_MOTION = window.matchMedia("(prefers-reduced-motion: reduce)");
 const DARK_SCHEME = window.matchMedia("(prefers-color-scheme: dark)");

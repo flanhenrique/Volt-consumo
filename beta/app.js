6c82578fc4182ad369cb8bb20823b481977bf411
flanhenrique <walflanribeiro@gmail.com>
2026-08-12 14:44:25 -0400

    Reverte atualização incompleta de app.js

--- beta/app.js
+++ beta/app.js
@@ -1,1 +1,8 @@
-PLACEHOLDER
+import {
+  calculateConsumptionSummary,
+  calculateEnergyEstimate,
+  calculateGoalProgress,
+  calculateWaterEstimate,
+  detectContinuousWaterFlow,
+  forecastLegacyLinear
+} from "./packages/consumption-domain/browser/index.js";

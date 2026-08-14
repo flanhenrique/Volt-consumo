c0d9256e7b7e862be08bd41a25bab53f46f39152
flanhenrique <walflanribeiro@gmail.com>
2026-08-14 12:38:13 -0400

    chore: align auxiliary modules with build 20260814.15

--- config.js
+++ config.js
@@ -4,10 +4,10 @@ export const VOLT_CONFIG = Object.freeze({
 });
 
 const AUXILIARY_MODULES = Object.freeze([
-  "./src/consumption-reports.js?v=20260814.13",
-  "./src/home-dashboard-v2.js?v=20260814.13",
-  "./src/home-dashboard-sustainability.js?v=20260814.13",
-  "./src/pwa-update.js?v=20260814.13"
+  "./src/consumption-reports.js?v=20260814.15",
+  "./src/home-dashboard-v2.js?v=20260814.15",
+  "./src/home-dashboard-sustainability.js?v=20260814.15",
+  "./src/pwa-update.js?v=20260814.15"
 ]);
 
 function loadAuxiliaryModules() {

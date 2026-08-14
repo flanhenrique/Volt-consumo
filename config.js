2a6cd2146e633db5e56204f0989524577429c533
flanhenrique <walflanribeiro@gmail.com>
2026-08-14 01:12:41 -0400

    Atualiza versão da Home sem consumo diário

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 import "./src/consumption-reports.js?v=20260813.7";
 import "./src/home-dashboard-v2.js?v=20260814.1";
-import "./src/home-dashboard-sustainability.js?v=20260814.2";
+import "./src/home-dashboard-sustainability.js?v=20260814.3";
 
 export const VOLT_CONFIG = Object.freeze({
   url: "https://zatwcrouojjqnuuabdif.supabase.co",

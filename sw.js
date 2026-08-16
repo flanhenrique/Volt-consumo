13449e476efd885add525e386f42a05d2c2fc090
flanhenrique <walflanribeiro@gmail.com>
2026-08-15 20:42:22 -0400

    chore: rotate PWA cache for build 20260815.17

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
 const RELEASE_ID = "20260813.7";
-const CACHE_REVISION = "20260815.16";
+const CACHE_REVISION = "20260815.17";
 const CACHE_NAME = `volt-app-v4-atomic-${CACHE_REVISION}`;
 const BOOTSTRAP_BUILD = "20260815.10";
-const UPDATE_BUILD = "20260815.16";
+const UPDATE_BUILD = "20260815.17";
 const OWNED_CACHE_NAMES = new Set([
   CACHE_NAME,
+  "volt-app-v4-atomic-20260815.16",
   "volt-app-v4-atomic-20260815.15",
   "volt-app-v4-atomic-20260815.14",
   "volt-app-v4-atomic-20260815.13",

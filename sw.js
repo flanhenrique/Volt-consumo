fa9350bddce1123799dd7014040a4f6a54abe55f
flanhenrique <walflanribeiro@gmail.com>
2026-08-15 20:31:33 -0400

    chore: refresh PWA cache for Android easter target

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
 const RELEASE_ID = "20260813.7";
-const CACHE_REVISION = "20260815.14";
+const CACHE_REVISION = "20260815.15";
 const CACHE_NAME = `volt-app-v4-atomic-${CACHE_REVISION}`;
 const BOOTSTRAP_BUILD = "20260815.10";
-const UPDATE_BUILD = "20260815.14";
+const UPDATE_BUILD = "20260815.15";
 const OWNED_CACHE_NAMES = new Set([
   CACHE_NAME,
+  "volt-app-v4-atomic-20260815.14",
   "volt-app-v4-atomic-20260815.13",
   "volt-app-v4-atomic-20260815.12",
   "volt-app-v4-atomic-20260815.11",
